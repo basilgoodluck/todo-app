03b228cea55574dd7c8fad73ce228a574556bd5b
Basil Goodluck <basilgoodluck22@gmail.com>
2024-04-16 04:01:27 +0100

    Updated

--- src/components/todoBody.jsx
+++ src/components/todoBody.jsx
@@ -1,7 +1,6 @@
 import TodoTracker from "./todoTracker";
 import "../styles/todoBody.css"
 import TodoForm from "./todoForm";
-import Todo from "./todo";
 import { useState } from "react";
 import TodoWrapper from './todoWrapper'
 
@@ -20,12 +19,17 @@ export default function TodoBody() {
     
   }
   
+  const handleSubmit = function (e) {
+    e.preventDefault()
+
+}
   return (
     <div className="todoBody--main">
       <TodoTracker />
       <TodoForm 
         task = {formData.task}
         getTask = {getTask}
+        handleSubmit={handleSubmit}
       />
       <TodoWrapper />
     </div>
--- src/components/todoWrapper.jsx
+++ src/components/todoWrapper.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import "../styles/todoWrapper.css"
+import Todo from "./todo";
+
 
 function TodoWrapper() {
+
+    const anArray = ["We are glad to have you", "Love the way you lie", "Who is not happy"]
+    const funct = anArray.map((e)=>{
+        return <Todo todo={e}/>
+    })
+
   return (
     <div className='todoWrapper'>
-              
+        {funct} 
     </div>
   )
 }
