33b022a2ac266b1110183c54b540d36892f3a200
Basil Goodluck <basilgoodluck22@gmail.com>
2024-04-23 11:26:18 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/header.js'
-import "./app.css"
+import "./App.css"
 import TodoWrapper from './components/todoWrapper.js'
 
 const App = () => {
